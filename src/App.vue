4bbfa9013fae8d71d18857f6dcafbd363151d9f7
Tofusupreme <Yunishbacon@gmail.com>
2021-06-05 09:11:18 +0900

    Updated functionality

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,49 @@
     <div class="grid_container">
         <div class="left_panel">
             <span class="messenger_Logo">Messenger</span>
-                <div class="room_notification">
-                    <p>Person A has entered the room</p>
-                    <p>Person B has entered the room</p>
-                    <p>Person C has left the room</p>
-                </div>
-                <input type="text" class="left_panel_text_input" name="left_panel_text_input" id="left_panel_text_input" placeholder="search">
+
+            <!-- Display the rooms list -->
+            <ul class="room_list">
+                <li class="room"
+                    @click="room_switch(room.id)"
+                    v-for="room in rooms"
+                    :key="room.id">
+                    {{ room.users[0] }}
+                    {{ room.messages[room.messages.length - 1].text }} 
+                    <!-- With a given array, it grabs the last value of that array -->
+                </li>
+            </ul>
         </div>
-    <div class="right_panel">
+
+        <div class="right_panel">
             <div class="user_header">
                 <span class="userName_ID">User Name</span>
             </div>
+
             <ul class="chat_box">
-                <li class="chat_box_messages"
-                    v-for="(message, index) in messages" :key="index">
-                    {{ message }}
+                <li 
+                    class="chat_box_messages"
+                    v-for="(message, index) in rooms[selectedRoom].messages" 
+                    :key="index">
+                    {{ message.from }}: {{ message.text }}
                 </li>
             </ul>
-                <input 
-                    type="text" 
-                    class="userMessage_text" 
-                    name="userMessage_text" 
-                    id="userMessage_text" 
-                    placeholder="Enter message here ..."
-                    v-model="input"
-                />
-                <button @click="onSubmit">Click On Me</button>
-            </div>
+
+            <input 
+                type="text" 
+                class="userMessage_text" 
+                name="userMessage_text" 
+                id="userMessage_text" 
+                placeholder="Enter message here ..."
+                v-model="input"
+            />
+
+            <button @click="onSubmit">Click On Me</button>
+        </div>
     </div>
 </template>
 
 <script>
-
-
 export default {
     name: 'App',
     components: {
@@ -43,15 +53,79 @@ export default {
     data() {
         return {
             input: "",
-            messages: []
+            selectedRoom: '1',
+            rooms: {
+                '1': {
+                    id: '1',
+                    users: ['fakeUserName','self'], // Strings
+                    messages: [ // Objects
+                        {
+                            from: 'fakeUserName',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'fakeUserName',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'self',
+                            text: 'I see you got text'
+                        },
+                        {
+                            from: 'fakeUserName',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'fakeUserName',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'fakeUserName',
+                            text: 'New Text'
+                        },
+                    ]
+                },
+                '2': {
+                    id: '2',
+                    users: ['fakeUserName2','self'], // Strings
+                    messages: [ // Objects
+                        {
+                            from: 'fakeUserName2',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'fakeUserName2',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'self',
+                            text: 'I see you got text'
+                        },
+                        {
+                            from: 'fakeUserName2',
+                            text: 'Some text'
+                        },
+                        {
+                            from: 'fakeUserName2',
+                            text: 'Some text'
+                        },
+                    ]
+                },
+            },
         }
     },
     methods: {
+        room_switch(newRoom) {
+            this.selectedRoom = newRoom
+        },
         onSubmit() {
-            this.messages.push(this.input)
+            this.rooms[this.selectedRoom].messages.push({
+                from: 'self',
+                text: this.input
+            })
             this.input = ""
         }
-    },
+    }
 }
 </script>
 
@@ -67,11 +141,11 @@ body {
 
 .grid_container {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: 1fr 2fr;
     grid-template-rows: 1fr;
     grid-column-gap: 0px;
     grid-row-gap: 0px;
-    height: 1024px;
+    height: 100vh;
 }
 
 .left_panel {
@@ -79,7 +153,7 @@ body {
     flex-direction: column;
     background: rgba(212, 216, 216, 0.48);
     opacity: 1;
-    overflow: hidden;
+    overflow: auto;
 }
 
 .messenger_Logo {
@@ -90,23 +164,6 @@ body {
     opacity: 1;
     text-align: center;
     margin-top: 50px;
-}
-
-.room_notification {
-    margin: 100px 0 0 200px;
-    width: 250px;
-    height: 250px;
-    background: rgba(255, 255, 255, 1);
-    border: 2px solid black;
-}
-.left_panel_text_input {
-    margin: 100px 0 0 100px;
-    width: 400px;
-    height: 52px;
-    background: rgba(255, 255, 255, 1);
-    opacity: 1;
-    border-radius: 10px;
-    border: none;
 }
 
 .right_panel {
