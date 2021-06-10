af6040171f78ba8bb370df2d6c827ad075dbc487
Tofusupreme <Yunishbacon@gmail.com>
2021-06-10 11:35:56 +0900

    Updated styles to LeftPanel

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,26 @@
 
 <template>
     <div class="grid_container">
-        <div class="left_panel">
-            <div class="meseenger_header">
-            <span class="messenger_logo">
-            <font-awesome-icon icon="user-circle" class="profile_icon"/>
-                    Chat
-                    </span>
-                    </div>
-                
-            <!-- Display the rooms list -->
-            <ul class="room_list">
-                <li class="room"
-                    @click="room_switch(room.id)"
-                    v-for="room in rooms"
-                    :key="room.id">
-                    {{ room.users[0] }}
-                    {{ room.messages[room.messages.length - 1].text }} 
-                    <!-- With a given array, it grabs the last value of that array -->
-                </li>
-            </ul>
-        </div>
+        <LeftPanel :rooms="rooms" @roomSwitch="room_switch"/>
+        <RightPanel :rooms="rooms" :selectedRoom="selectedRoom" 
+            @onSubmit="onSubmit"/>
 
-        <div class="right_panel">
-            <div class="user_header">
-                <span class="userName_ID">
-                    <font-awesome-icon icon="user"/>
-                    User Name</span>
-            </div>
-            <div class="messenger_body">
-            <ul class="chat_box">
-                <li 
-                    class="chat_box_messages"
-                    v-for="(message, index) in rooms[selectedRoom].messages" 
-                    :key="index">
-                    {{ message.from }}: {{ message.text }}
-                </li>
-            </ul>
-
-            <input 
-                type="text" 
-                class="userMessage_text" 
-                name="userMessage_text" 
-                id="userMessage_text" 
-                placeholder="Enter message here ..."
-                v-model="input"
-            />
-            <button @click="onSubmit" class="messenger_button">Send</button> 
-            </div>
-        </div>
+        <!-- @eventName="functionName" -->
     </div>
 </template>
 
 <script>
+import LeftPanel from './components/LeftPanel.vue'
+import RightPanel from './components/RightPanel.vue'
 
 export default {
     name: 'App',
     components: {
-
+        LeftPanel, 
+        RightPanel,
     },
-    
     data() {
         return {
-            input: "",
             selectedRoom: '1',
             rooms: {
                 '1': {
@@ -128,12 +86,11 @@ export default {
         room_switch(newRoom) {
             this.selectedRoom = newRoom
         },
-        onSubmit() {
+        onSubmit(input) {
             this.rooms[this.selectedRoom].messages.push({
                 from: 'self',
-                text: this.input
+                text: input
             })
-            this.input = ""
         }
     }
 }
@@ -158,117 +115,4 @@ body {
     height: 100vh;
 }
 
-.left_panel {
-    display: flex;
-    flex-direction: column;
-    opacity: 1;
-    overflow: auto;
-}
-
-.meseenger_header {
-    background:#ffc3d7;
-    padding: 40px;
-}
-.messenger_logo {
-    color: rgba(0, 0, 0, 1);
-    font-family: Roboto;
-    font-weight: Bold;
-    font-size: 28px;
-    opacity: 1;
-    padding: 40px;
-
-}
-
-.profile_icon {
-    padding-right: 10px;
-    margin-right: 50px;
-}
-
-.room_list {
-padding-top: 30px;
-}
-
-.room_list:hover {
-    cursor: pointer;
-}
-.room {
-    padding: 20px;
-    list-style: none;
-    font-size: 14px;
-    font-style: italic;
-}
-
-.right_panel {
-        display: flex;
-        flex-direction: column;
-        opacity: 1;
-        overflow: hidden;
-        background-color: #c03563;
-        
-}
-
-.user_header {
-    padding: 35px;
-    background-color: #c03563
-}
-.userName_ID {
-    color: rgba(0, 0, 0, 1);
-    font-family: Roboto;
-    font-weight: Bold;
-    font-size: 36px;
-    margin-left: 100px;
-    opacity: 1;
-}
-.messenger_body {
-    background:#ffc3d7;
-}
-.chat_box {
-    margin: 50px 0 0 200px;
-    width: 225px;
-    height: 250px;
-    padding-bottom: 30px;
-    padding-top: 15px;
-    background-color: white;
-}
-
-.chat_box_messages {
-    list-style: none;
-    text-align: left;
-    margin-top:15px;
-}
-
-.userMessage_text {
-    width: 400px;
-    height: 62px;
-    margin: 100px;
-    opacity: 1;
-    overflow: hidden;
-    border-radius: 30px;
-}
-
-input[type="text"] {
-    padding-left: 15px;
-    font-size: 16px;
-}
-
-input[type="text"]:focus {
-    outline: none;
-    -webkit-box-shadow: 1px 2px 9px 1px #000000;
-    box-shadow: 1px 2px 9px 1px #000000;
-
-}
-
-.messenger_button {
-    background-color: white;
-    border-radius: 28px;
-    height: 40px;
-    width: 125px;
-    margin-left: -50px;
-    display: inline-block;
-    cursor: pointer;
-    color: #c03563;
-    font-size: 16px;
-    text-decoration: none;
-    text-shadow: 0px 1px 0px black;
-}
 </style>
---
+++ src/components/RightPanel.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="right_panel">
+        <div class="user_header">
+            <span class="userName_ID">
+                <font-awesome-icon icon="user"/>
+                User Name
+            </span>
+        </div>
+
+        <div class="messenger_body">
+            <ul class="chat_box">
+                <li 
+                    class="chat_box_messages"
+                    v-for="(message, index) in rooms[selectedRoom].messages" 
+                    :key="index">
+                    {{ message.from }}: {{ message.text }}
+                </li>
+            </ul>
+
+            <input 
+                type="text" 
+                class="userMessage_text" 
+                name="userMessage_text" 
+                id="userMessage_text" 
+                placeholder="Enter message here ..."
+                v-model="input"
+            />
+            <button @click="handleClick" class="messenger_button">
+                Send
+            </button> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RightPanel',
+    props: {
+        rooms: Object,
+        selectedRoom: String,
+    },
+    data(){
+        return {
+            input: ''
+        }
+    },
+    methods: {
+        handleClick(input) { 
+            this.$emit('onSubmit', this.input)
+            this.input = '' 
+            console.log(input)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.right_panel {
+        display: flex;
+        flex-direction: column;
+        opacity: 1;
+        overflow: hidden;
+        background-color: #c03563;
+        
+}
+
+.user_header {
+    padding: 35px;
+    background-color: #c03563
+}
+.userName_ID {
+    color: rgba(0, 0, 0, 1);
+    font-family: Roboto;
+    font-weight: Bold;
+    font-size: 36px;
+    margin-left: 100px;
+    opacity: 1;
+}
+.messenger_body {
+    background:#ffc3d7;
+}
+.chat_box {
+    margin: 50px 0 0 200px;
+    width: 225px;
+    height: 250px;
+    padding-bottom: 30px;
+    padding-top: 15px;
+    background-color: white;
+}
+
+.chat_box_messages {
+    list-style: none;
+    text-align: left;
+    margin-top:15px;
+}
+
+.userMessage_text {
+    width: 400px;
+    height: 62px;
+    margin: 100px;
+    opacity: 1;
+    overflow: hidden;
+    border-radius: 30px;
+}
+
+input[type="text"] {
+    padding-left: 15px;
+    font-size: 16px;
+}
+
+input[type="text"]:focus {
+    outline: none;
+    -webkit-box-shadow: 1px 2px 9px 1px #000000;
+    box-shadow: 1px 2px 9px 1px #000000;
+
+}
+
+.messenger_button {
+    background-color: white;
+    border-radius: 28px;
+    height: 40px;
+    width: 125px;
+    margin-left: -50px;
+    display: inline-block;
+    cursor: pointer;
+    color: #c03563;
+    font-size: 16px;
+    text-decoration: none;
+    text-shadow: 0px 1px 0px black;
+}
+
+</style>
